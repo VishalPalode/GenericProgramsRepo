b33a26c07aa118e47ff6e2b0a51888b077a1f6d2
vishal <vishalpalode9899@gmail.com>
2022-02-02 16:55:03 +0530

    FindMaxInt

--- GenericProgram/Program.cs
+++ GenericProgram/Program.cs
@@ -10,7 +10,12 @@ namespace GenericProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to generic program");
+            //Instanse variables.
+            int first = 50, second = 200, third = 100;
+
+            //Creating object of FindMaxValueClass.
+            FindMaxValue findMaxValue = new FindMaxValue();
+            Console.WriteLine("Max number is:{0} out of numbers : {1} , {2} , {3}", findMaxValue.FindMaxInteger(first, second, third), first, second, third);
             Console.ReadLine();
         }
     }
